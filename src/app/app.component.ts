73b724947fcd999564e466245049f8564b76fa6f
Aman Mahendroo <30946991+amanMahendroo@users.noreply.github.com>
2020-07-10 16:04:58 +0530

    Angular to-do list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular';
+	title = 'AngularBasics';
+	list = []
+
+	remove(index) {
+		this.list.splice(index, 1)
+	}
+
+	add(event) {
+		if (event.keyCode == 13 && event.target.value.length > 0) {
+			this.list.push(event.target.value)
+			event.target.value = ""
+		}
+	}
 }

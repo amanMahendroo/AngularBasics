58894305c74fd596eea0d4f0983fb238adfdc4c7
Aman Mahendroo <30946991+amanMahendroo@users.noreply.github.com>
2020-07-11 13:48:58 +0530

    Status for items
    Now you can put a status to your to-do list events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,9 +5,11 @@ import { Component } from '@angular/core';
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
 	title = 'AngularBasics';
-	list = []
+	list = [{content: "1", status: 0, color: "transparent"}]
+	colors = ["transparent", "#0f8", "#ff0", "#f80", "#f00"]
 
 	remove(index) {
 		this.list.splice(index, 1)
@@ -15,8 +17,16 @@ export class AppComponent {
 
 	add(event) {
 		if (event.keyCode == 13 && event.target.value.length > 0) {
-			this.list.push(event.target.value)
+			this.list.push({content: event.target.value, status: 0, color: "transparent"})
 			event.target.value = ""
 		}
 	}
+
+	addStat(i, j) {
+		this.list[i].status = j
+		this.list[i].color = "5px solid " + this.colors[this.list[i].status];
+		// (document.querySelector('.app-alerts') as HTMLElement).style.top = '150px';
+		// console.log(this.list[i])
+	}
 }
+
